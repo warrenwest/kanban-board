568c5c185afcbbdad7f8248af6a1eeb9f8302666
Wren West <wrenwestart@gmail.com>
2025-05-07 12:52:57 -0400

    edited models/index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,15 +5,21 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || 'default_db_name',
+    process.env.DB_USERNAME || 'default_db_user',
+    process.env.DB_PASSWORD || 'default_db_password',
+    {
       host: 'localhost',
       dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    },
+  );
+}
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
